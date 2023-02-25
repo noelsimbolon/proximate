59f0ba4db35debafed4c29df540cffc26ce41c97
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-02-25 09:30:21 +0700

    feat: divide and conquer with restriction handled

--- src/process.py
+++ src/process.py
@@ -4,7 +4,7 @@ import math
 # dvdBy = 0 , represent divide by x
 # dvdBy = 1, represent divide by y
 def dnq(vectors, dimension, dvdBy=0):
-    if (len(vectors) > 2): # recurence
+    if (len(vectors) > 3): # recurence
 
         # sort vectors
         sorted_vectors = sort(vectors, dvdBy)
@@ -17,14 +17,59 @@ def dnq(vectors, dimension, dvdBy=0):
         vectors_right = sorted_vectors[divideAt:]
 
         # recurence
-        closest_left, dist_left = dnq(vectors_left, dimension, (dvdBy+1)%(dimension-1))
-        closest_right, dist_right = dnq(vectors_right, dimension, (dvdBy+1)%(dimension-1))
+        closest_left, dist_left, count_left = dnq(vectors_left, dimension, (dvdBy+1)%(dimension-1))
+        closest_right, dist_right, count_right = dnq(vectors_right, dimension, (dvdBy+1)%(dimension-1))
 
         # find minimum
         closest_vector, min_dist = (closest_left,dist_left) if dist_left < dist_right else (closest_right,dist_right)
 
+        # counter if there is point one point in left and one point in right that is nearer
+        base_left = vectors_left[-1][dvdBy]
+        base_right = vectors_right[0][dvdBy]
+
+        base_avg = (base_left+base_right)/2
+
+        border_left = base_avg-min_dist
+        border_right = base_avg+min_dist
+
+        # collect all points within border
+        count_gray = 0
+        vectors_gray = []
+
+        # collect from left side
+        i = 1;
+        while (i <= len(vectors_left) and vectors_left[-i][dvdBy] >= border_left):
+            vectors_gray.append(vectors_left[-i])
+            i+=1
+
+        # collect from right side
+        j = 0;
+        while (j < len(vectors_right) and vectors_right[j][dvdBy] <= border_right):
+            vectors_gray.append(vectors_right[j])
+            j+=1
+
+        # sort vectors_gray
+        vectors_gray_sorted = sort(vectors_gray, (dvdBy+1)%(dimension))
+
+        # flag variable to break
+        break_in_j = False
+        for i in range(len(vectors_gray_sorted)):
+            for j in range(i+1, len(vectors_gray_sorted)):
+                for k in range(dimension):
+                    if abs(vectors_gray_sorted[i][k] - vectors_gray_sorted[j][k]) > min_dist:
+                        break_in_j = True
+                        break
+                if break_in_j:
+                    break_in_j = False
+                    break
+                else:   
+                    temp_dist = distance((vectors_gray_sorted[i], vectors_gray_sorted[j]))
+                    count_gray += 1
+                    if (temp_dist < min_dist):
+                        closest_vector, min_dist = (vectors_gray_sorted[i], vectors_gray_sorted[j]), temp_dist
+    
         # return
-        return closest_vector, min_dist
+        return closest_vector, min_dist, count_left+count_right+count_gray
 
         # repeat
         # sort by x
@@ -32,12 +77,26 @@ def dnq(vectors, dimension, dvdBy=0):
         # sort by y
         # divide by y
 
-    else: # basis
+    elif (len(vectors) == 3): # basis 1
+        vect_temp1, dist1 = vectors[:2], distance(vectors[:2])
+        vect_temp2, dist2 = vectors[1:3], distance(vectors[1:3])
+        vect_temp3, dist3 = (vectors[0],vectors[2]), distance((vectors[0],vectors[2]))
+
+        min_dist_temp = min(dist1, dist2, dist3)
+
+        if min_dist_temp == dist1:
+            return vect_temp1, dist1, 3
+        elif min_dist_temp == dist2:
+            return vect_temp2, dist2, 3
+        else:
+            return vect_temp3, dist3, 3
+
+    else : # basis 2
         # find distance between 2 vectors
         dist = distance(vectors) 
 
         # return the two vectors and distance between them
-        return vectors, dist
+        return vectors, dist, 1
 
 # by = 0, sort by x axis
 # by = 1, sort by y axis
--- src/program.py
+++ src/program.py
@@ -16,12 +16,24 @@ VECTOR_DIMENSION = 3
 # Create array of tuple (x,y,z), randomly, with each point between -100 and 100
 vectors = [tuple(random.uniform(-100, 100) for _ in range(VECTOR_DIMENSION)) for _ in range(number_of_points)]
 
+vectors = [(50.572799210020094, 83.15402995504587, 43.57900364087348), 
+           (-28.885983623645558, -1.5488497740578708, 10.598584194109065), 
+           (-89.61165627908294, 14.165609770503067, -79.31865904879712), 
+           (-12.535935067584262, 46.42547937023019, 99.9688164471481), 
+           (41.88775573477139, 56.67617235079277, 20.51898520442208), 
+           (0.5414408505394448, -82.30758932481517, -41.19101128982159), 
+           (-73.0334115919971, 36.76972671749243, 51.30924915733209), 
+           (80.27710476887856, 34.412885675091786, 95.87915684444215), 
+           (14.505662446880606, 28.093157016376438, -24.31732344755673), 
+           (32.41555972051634, 44.64304393191881, 98.14785777564083)]
+
 # Print points that have been generated
 print(color.green("\n========POINTS========="))
 print(color.yellow(vectors))
 
 print()
 
-closest_vectors, dist = pcs.dnq(vectors, VECTOR_DIMENSION)
+closest_vectors, dist, count = pcs.dnq(vectors, VECTOR_DIMENSION)
 print(color.magenta(f"Closest vector   : {closest_vectors}"))
 print(color.blue(f"Distance         : {dist}" ))
+print(color.cyan(f"Count            : {count}" ))
