8aac866dd25b1779ae323a82dbcd3f06d3b441ae
noelsimbolon <84700640+noelsimbolon@users.noreply.github.com>
2023-02-27 06:53:38 +0700

    refactor: finalize brute force

--- src/brute_force.py
+++ src/brute_force.py
@@ -5,27 +5,27 @@ import numpy as np
 import util
 
 
-def find_closest_pair_bf(vectors: np.ndarray[np.ndarray[float]]) -> \
+def find_closest_pair_bf(points: np.ndarray[np.ndarray[float]]) -> \
         Tuple[Tuple[np.ndarray[float], np.ndarray[float]], float, int]:
     """
     Finds the closest pair of points using brute-force algorithm
-    :param vectors: a numpy array of floats
-    :return: a tuple: (the closest pair of points, minimum distance, number of Euclidean distance operations)
+    :param points: a numpy array of points
+    :return: a tuple: (the closest pair of points, its distance, number of Euclidean distance operations)
     """
 
     distance_count = 0
     min_dist = float('inf')
     closest_pair = None
-    n = len(vectors)
+    n = len(points)
 
     for i in range(n):
         for j in range(i + 1, n):
 
-            dist = util.euclidean_distance(vectors[i], vectors[j])
+            dist = util.euclidean_distance(points[i], points[j])
             distance_count += 1
 
             if dist < min_dist:
                 min_dist = dist
-                closest_pair = (vectors[i], vectors[j])
+                closest_pair = (points[i], points[j])
 
     return closest_pair, min_dist, distance_count
