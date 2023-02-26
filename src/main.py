c0b70083c5d2388a8b59d2d99bc104edb9ffed76
noelsimbolon <84700640+noelsimbolon@users.noreply.github.com>
2023-02-27 06:55:49 +0700

    feat: integrate GUI with backend

--- src/main.py
+++ src/main.py
@@ -1,25 +1,40 @@
-import input_handling
+import random
+from timeit import default_timer as timer
+
 import customtkinter
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from timeit import default_timer as timer
+
+import brute_force as bf
+import divide_and_conquer as dnc
+import util
 
 customtkinter.set_appearance_mode("Dark")
 customtkinter.set_default_color_theme("blue")
 
 
 class App(customtkinter.CTk):
+    # initialize inputs
+    number_of_dimensions = 0
+    number_of_points = 0
+    points = None
+
+    # Output variables for divide-and-conquer
+    # ed_operations means the number of euclidean distance operations performed
+    dnc_ed_operations = ""
+    dnc_execution_time = ""
+    dnc_closest_pair = None
+    dnc_distance = None
+
+    # Output variables for brute-force
+    bf_ed_operations = ""
+    bf_execution_time = ""
+    bf_closest_pair = None
+    bf_distance = None
 
     def __init__(self):
         super().__init__()
-
-        # Output variables for divide-and-conquer
-        dnc_ed_operations = ""
-        dnc_execution_time = ""
-
-        # Output variables for brute-force
-        bf_ed_operations = ""
-        bf_execution_time = ""
 
         # Font variables
         title_font = customtkinter.CTkFont(family="Arial Bold", size=-18)
@@ -27,6 +42,7 @@ class App(customtkinter.CTk):
         output_heading_font = customtkinter.CTkFont(family="Arial Bold", size=-14)
         placeholder_font = customtkinter.CTkFont(family="Arial", size=-12)
         select_theme_font = customtkinter.CTkFont(family="Arial", size=-12)
+        randomize_input_font = customtkinter.CTkFont(family="Arial Bold", size=-12)
         start_button_font = customtkinter.CTkFont(family="Arial Bold", size=-12)
         status_font = customtkinter.CTkFont(family="Arial", size=-12)
         validation_label_font = customtkinter.CTkFont(family="Arial", size=-12)
@@ -49,12 +65,12 @@ class App(customtkinter.CTk):
 
         # ============ left_frame ============
 
-        # Configure grid layout (18 x 1)
+        # Configure grid layout (19 x 1)
         self.left_frame.grid_rowconfigure(0, minsize=10)  # empty row with minsize as spacing
         self.left_frame.grid_rowconfigure(2, minsize=20)  # space between input fields and app title
-        self.left_frame.grid_rowconfigure(11, weight=1)  # empty row as spacing
-        self.left_frame.grid_rowconfigure(14, minsize=20)  # empty row as spacing
-        self.left_frame.grid_rowconfigure(17, minsize=20)  # empty row with minsize as spacing
+        self.left_frame.grid_rowconfigure(12, weight=1)  # empty row as spacing
+        self.left_frame.grid_rowconfigure(15, minsize=20)  # empty row as spacing
+        self.left_frame.grid_rowconfigure(18, minsize=20)  # empty row with minsize as spacing
 
         # App title
         self.app_title = customtkinter.CTkLabel(master=self.left_frame,
@@ -96,41 +112,47 @@ class App(customtkinter.CTk):
                                                                             font=validation_label_font)
         self.number_of_dimensions_validation_label.grid(row=8, column=0, pady=(0, 10), padx=20)
 
+        # Randomize input button
+        self.randomize_input_button = customtkinter.CTkButton(master=self.left_frame,
+                                                              text="Randomize Input",
+                                                              font=randomize_input_font,
+                                                              command=self.randomize_input)
+        self.randomize_input_button.grid(row=9, column=0, pady=5, padx=20)
+
         # Start button
         self.start_button = customtkinter.CTkButton(master=self.left_frame,
                                                     text="Start",
                                                     font=start_button_font,
                                                     command=self.start)
-
-        self.start_button.grid(row=9, column=0, pady=5, padx=20)
+        self.start_button.grid(row=10, column=0, pady=5, padx=20)
 
         self.space_label = customtkinter.CTkLabel(master=self.left_frame,
                                                   text="")
-        self.space_label.grid(row=10, column=0, pady=5, padx=20)
+        self.space_label.grid(row=11, column=0, pady=5, padx=20)
 
         # Result
         self.status_heading_label = customtkinter.CTkLabel(master=self.left_frame,
                                                            text="Status:",
                                                            font=output_heading_font)
-        self.status_heading_label.grid(row=12, column=0, pady=0, padx=20)
+        self.status_heading_label.grid(row=13, column=0, pady=0, padx=20)
 
         # Prints the status of the program
         self.status_label = customtkinter.CTkLabel(master=self.left_frame,
                                                    text="",
                                                    font=status_font)
-        self.status_label.grid(row=13, column=0, pady=0, padx=20)
+        self.status_label.grid(row=14, column=0, pady=0, padx=20)
 
         # Select GUI theme
         self.theme_label = customtkinter.CTkLabel(master=self.left_frame,
                                                   text="Select theme:",
                                                   font=select_theme_font)
-        self.theme_label.grid(row=15, column=0, pady=0, padx=20, sticky="s")
+        self.theme_label.grid(row=16, column=0, pady=0, padx=20, sticky="s")
 
         self.theme_options = customtkinter.CTkOptionMenu(master=self.left_frame,
                                                          values=["Dark", "Light", "System"],
                                                          font=select_theme_font,
                                                          command=change_appearance_mode)
-        self.theme_options.grid(row=16, column=0, pady=5, padx=20, sticky="")
+        self.theme_options.grid(row=17, column=0, pady=5, padx=20, sticky="")
 
         # ============ right_frame ============
 
@@ -145,6 +167,14 @@ class App(customtkinter.CTk):
         # Frame containing matplotlib 3D scatter plot (only shows up if the number of dimensions is 3)
         self.visualization_frame = customtkinter.CTkFrame(master=self.right_frame, fg_color="transparent")
         self.visualization_frame.grid(row=1, column=1, pady=(20, 20), padx=(20, 20), sticky="nswe")
+
+        # Initialize a canvas for 3D scatter plot
+        # This initialization is useful to avoid displaying multiple plots
+        # by destroying the canvas before creating a new one every time a plot is want to be drawn
+        self.visualization_canvas = FigureCanvasTkAgg(None, master=self.visualization_frame)
+
+        # Initialize label for output when number of dimensions != 3
+        self.points_output_label = customtkinter.CTkLabel(master=self.visualization_frame)
 
         # Output frame: contains output comparison between divide-and-conquer and brute-force
         self.output_frame = customtkinter.CTkFrame(master=self.right_frame)
@@ -168,13 +198,13 @@ class App(customtkinter.CTk):
         self.divide_and_conquer_label.grid(row=1, column=0, padx=(20, 10), pady=(0, 20))
 
         self.divide_and_conquer_output_label = customtkinter.CTkLabel(master=self.output_frame,
-                                                                      text=f'{dnc_ed_operations}\n'
-                                                                           f'{dnc_execution_time}',
+                                                                      text=f'{App.dnc_ed_operations}\n'
+                                                                           f'{App.dnc_execution_time}',
                                                                       font=output_font,
                                                                       text_color="green",
                                                                       anchor="w",
                                                                       justify="left")
-        self.divide_and_conquer_output_label.grid(row=1, column=1, padx=(20, 10), pady=(0, 20))
+        self.divide_and_conquer_output_label.grid(row=1, column=1, padx=(0, 10), pady=(0, 20))
 
         # output labels for brute-force algorithm
         self.brute_force_heading_label = customtkinter.CTkLabel(master=self.output_frame,
@@ -192,29 +222,29 @@ class App(customtkinter.CTk):
         self.brute_force_label.grid(row=1, column=2, padx=(20, 10), pady=(0, 20))
 
         self.brute_force_output_label = customtkinter.CTkLabel(master=self.output_frame,
-                                                               text=f'{bf_ed_operations}\n'
-                                                                    f'{bf_execution_time}',
+                                                               text=f'{App.bf_ed_operations}\n'
+                                                                    f'{App.bf_execution_time}',
                                                                font=output_font,
                                                                text_color="green",
                                                                anchor="w",
                                                                justify="left")
-        self.brute_force_output_label.grid(row=1, column=3, padx=(20, 10), pady=(0, 20))
+        self.brute_force_output_label.grid(row=1, column=3, padx=(0, 10), pady=(0, 20))
 
-        # Create the plot
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.scatter([1, 2, 3], [4, 5, 6], [7, 8, 9])
-
-        # Add the plot to a Tkinter canvas
-        canvas = FigureCanvasTkAgg(fig, master=self.visualization_frame)
-        canvas.draw()
-        canvas.get_tk_widget().pack()
-
-    def print_output(self):
+    def randomize_input(self):
         """
-        Display Euclidean distance operations and execution time output in the GUI
+        Randomizes input for the number of dimensions and the number of points
         """
-        # self.execution_time_label.configure(text=App.execution_time)
+        # Generate random integer
+        App.number_of_dimensions = random.randint(1, 50)
+        App.number_of_points = random.randint(2, 1000)
+
+        # Clear entry first
+        self.number_of_dimensions_entry.delete(0, 4)
+        self.number_of_points_entry.delete(0, 4)
+
+        # Then, insert the generated random integer
+        self.number_of_dimensions_entry.insert(0, str(App.number_of_dimensions))
+        self.number_of_points_entry.insert(0, str(App.number_of_points))
 
     def start(self):
         """
@@ -222,20 +252,20 @@ class App(customtkinter.CTk):
         """
         # if the number of dimensions or the number of points is not valid,
         # display error message until it is valid
-        if not (input_handling.validate_number_of_dimensions(self.number_of_dimensions_entry.get()) and
-                input_handling.validate_number_of_points(self.number_of_points_entry.get())):
+        if not (util.validate_number_of_dimensions(self.number_of_dimensions_entry.get()) and
+                util.validate_number_of_points(self.number_of_points_entry.get())):
 
             # clear status label if input is invalid
             self.status_label.configure(text="")
 
             # if the number of dimensions is not valid
-            if not input_handling.validate_number_of_dimensions(self.number_of_dimensions_entry.get()):
+            if not util.validate_number_of_dimensions(self.number_of_dimensions_entry.get()):
                 self.number_of_dimensions_validation_label.configure(text="Invalid number of dimensions.")
             else:
                 self.number_of_dimensions_validation_label.configure(text="")
 
             # if the number of points is not valid
-            if not input_handling.validate_number_of_points(self.number_of_points_entry.get()):
+            if not util.validate_number_of_points(self.number_of_points_entry.get()):
                 self.number_of_points_validation_label.configure(text="Invalid number of points.")
             else:
                 self.number_of_points_validation_label.configure(text="")
@@ -246,20 +276,124 @@ class App(customtkinter.CTk):
         self.number_of_dimensions_validation_label.configure(text="")
         self.number_of_points_validation_label.configure(text="")
 
+        # Assign inputs
+        App.number_of_dimensions = int(self.number_of_dimensions_entry.get())
+        App.number_of_points = int(self.number_of_points_entry.get())
+
+        # Generate an array of arrays containing float
+        App.points = np.random.uniform(low=-100, high=100, size=(App.number_of_points, App.number_of_dimensions))
+
         self.status_label.configure(text="Calculating...",
                                     text_color="yellow")
 
-        start_time = timer()
+        dnc_start_time = timer()
 
-        # do divide and conquer
+        App.dnc_closest_pair, App.dnc_distance, App.dnc_ed_operations = \
+            dnc.find_closest_pair_dnc(App.points, App.number_of_dimensions)
 
-        end_time = timer()
+        dnc_end_time = timer()
+
+        bf_start_time = timer()
+
+        App.bf_closest_pair, App.bf_distance, App.bf_ed_operations = bf.find_closest_pair_bf(App.points)
+
+        bf_end_time = timer()
+
+        # If the number of dimensions is 3, display 3D scatter plot
+        if App.number_of_dimensions == 3:
+            self.visualize()
+        else:
+            # Destroy visualization canvas (if a plot is present this destroys it)
+            self.visualization_canvas.get_tk_widget().destroy()
+            # Destroy text output
+            self.points_output_label.destroy()
+            self.points_output_label = customtkinter.CTkLabel(master=self.visualization_frame,
+                                                              text=f'Closest Pair of Points: '
+                                                                   f'{App.dnc_closest_pair[0]} and '
+                                                                   f'{App.dnc_closest_pair[1]}\n\n'
+                                                                   f'Euclidean Distance: '
+                                                                   f'{App.dnc_distance}',
+                                                              justify="left")
+            self.points_output_label.pack(padx=20, pady=20)
 
         self.status_label.configure(text="Done!",
                                     text_color="green")
 
-        elapsed_time = end_time - start_time
-        App.execution_time = f"{elapsed_time:.2f} s"
+        App.dnc_execution_time = f"{dnc_end_time - dnc_start_time:.5f} s"
+        App.bf_execution_time = f"{bf_end_time - bf_start_time:.5f} s"
+
+        self.show_ed_operations_and_execution_time()
+
+    def visualize(self):
+        """
+        Draw a 3D scatter plot if the number of dimensions is 3
+        """
+
+        first_point = App.dnc_closest_pair[0]
+        second_point = App.dnc_closest_pair[1]
+
+        # initialize arrays containing coordinates of points for each axis
+        x_coordinates = np.array([])
+        y_coordinates = np.array([])
+        z_coordinates = np.array([])
+
+        for i in range(App.number_of_points):
+            # if the current point is one of the points in the closest pair, don't append it
+            if any(np.array_equal(App.points[i], p) for p in App.dnc_closest_pair):
+                continue
+            x_coordinates = np.append(x_coordinates, App.points[i][0])
+            y_coordinates = np.append(y_coordinates, App.points[i][1])
+            z_coordinates = np.append(z_coordinates, App.points[i][2])
+
+        # Create the plot
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        # Scatter all points except the closest pair in blue
+        ax.scatter(x_coordinates, y_coordinates, z_coordinates, alpha=0.1, c='b')
+
+        # Scatter the closest pair of points in red
+        ax.scatter([first_point[0], second_point[0]],
+                   [first_point[1], second_point[1]],
+                   [first_point[2], second_point[2]], c='r')
+
+        # Draw a line between the closest pair of points
+        ax.plot([first_point[0], second_point[0]],
+                [first_point[1], second_point[1]],
+                [first_point[2], second_point[2]], c='k')
+
+        # Add labels and title
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        ax.set_title('3D Scatter Plot with Highlighted Closest Pair of Points')
+
+        # Add text annotations for closest pair of points and its distance
+        fig.text(0.03, 0.03, f'Closest Pair of Points: '
+                             f'({first_point[0]:.3f}, {first_point[1]:.3f}, {first_point[2]:.3f}) and '
+                             f'({second_point[0]:.3f}, {second_point[1]:.3f}, {second_point[2]:.3f})\n'
+                             f'Euclidean Distance: {App.dnc_distance:.3f}', fontsize=8, color='k')
+
+        # Destroy visualization canvas (if a plot is present this destroys it)
+        self.visualization_canvas.get_tk_widget().destroy()
+
+        # Destroy text output
+        if self.points_output_label.cget("text") != "CTkLabel" or "":
+            self.points_output_label.pack_forget()
+
+        # Then, draw a new plot to a Tkinter canvas
+        self.visualization_canvas = FigureCanvasTkAgg(fig, master=self.visualization_frame)
+        self.visualization_canvas.draw()
+        self.visualization_canvas.get_tk_widget().pack()
+
+    def show_ed_operations_and_execution_time(self):
+        """
+        Display Euclidean distance operations and execution time in the GUI
+        """
+        self.divide_and_conquer_output_label.configure(text=f'{App.dnc_ed_operations}\n'
+                                                            f'{App.dnc_execution_time}')
+        self.brute_force_output_label.configure(text=f'{App.bf_ed_operations}\n'
+                                                     f'{App.bf_execution_time}')
 
 
 def change_appearance_mode(new_appearance_mode: str):
