0cbacc368088cdd3812b337d4edc3cdf8181b794
noelsimbolon <84700640+noelsimbolon@users.noreply.github.com>
2023-02-27 04:08:22 +0700

    refactor: rename files to better reflect their usage

--- src/divide_and_conquer.py
+++ src/divide_and_conquer.py
@@ -1,13 +1,14 @@
 import math
 
+
 # divide_by means divide by either x-axis or y-axis.
 # divide_by = 0 , represent divide by x
 # divide_by = 1, represent divide by y
-def dnq(vectors, dimension, divide_by=0):
+def find_closest_pair_dnc(vectors, dimension, divide_by=0):
     if len(vectors) > 3:  # recurrence
 
         # sort vectors
-        sorted_vectors = quickSort(vectors, divide_by)
+        sorted_vectors = quick_sort(vectors, divide_by)
 
         # find index to divide
         divide_at = len(sorted_vectors) // 2
@@ -16,9 +17,13 @@ def dnq(vectors, dimension, divide_by=0):
         vectors_left = sorted_vectors[:divide_at]
         vectors_right = sorted_vectors[divide_at:]
 
-        # recurence
-        closest_left, dist_left, count_left = dnq(vectors_left, dimension, (divide_by+1)%(dimension-1 if dimension > 1 else dimension))
-        closest_right, dist_right, count_right = dnq(vectors_right, dimension, (divide_by+1)%(dimension-1 if dimension > 1 else dimension))
+        # recurrence
+        closest_left, dist_left, count_left = find_closest_pair_dnc(vectors_left, dimension,
+                                                                    (divide_by + 1) %
+                                                                    (dimension - 1 if dimension > 1 else dimension))
+        closest_right, dist_right, count_right = find_closest_pair_dnc(vectors_right, dimension,
+                                                                       (divide_by + 1) %
+                                                                       (dimension - 1 if dimension > 1 else dimension))
 
         # find minimum
         closest_vector, min_dist = (closest_left, dist_left) if dist_left < dist_right else (closest_right, dist_right)
@@ -26,25 +31,25 @@ def dnq(vectors, dimension, divide_by=0):
         # find if there were nearer points separated by the strip
         count_gray = 0
         for i in range(len(sorted_vectors)):
-            for j in range(i+1, len(sorted_vectors)):
-                check_futher = True
+            for j in range(i + 1, len(sorted_vectors)):
+                check_further = True
                 for k in range(dimension):
                     if abs(sorted_vectors[i][k] - sorted_vectors[j][k]) > min_dist:
-                        check_futher = False
+                        check_further = False
                         continue
-                if check_futher:
+                if check_further:
                     temp_dist = euclidean_distance((sorted_vectors[i], sorted_vectors[j]))
                     count_gray += 1
-                    if (temp_dist < min_dist):
+                    if temp_dist < min_dist:
                         closest_vector, min_dist = (sorted_vectors[i], sorted_vectors[j]), temp_dist
 
         # return
         return closest_vector, min_dist, count_left + count_right + count_gray
 
-    elif (len(vectors) == 3): # basis 1
+    elif len(vectors) == 3:  # basis 1
         vect_temp1, dist1 = vectors[:2], euclidean_distance(vectors[:2])
         vect_temp2, dist2 = vectors[1:3], euclidean_distance(vectors[1:3])
-        vect_temp3, dist3 = (vectors[0],vectors[2]), euclidean_distance((vectors[0],vectors[2]))
+        vect_temp3, dist3 = (vectors[0], vectors[2]), euclidean_distance((vectors[0], vectors[2]))
 
         min_dist_temp = min(dist1, dist2, dist3)
 
@@ -62,25 +67,29 @@ def dnq(vectors, dimension, divide_by=0):
         # return the two vectors and distance between them
         return vectors, dist, 1
 
+
 # distance of two vector
 def euclidean_distance(vectors):
-    point1, point2 = vectors
-    sumOfSquaredDelta = 0
+    first_point = vectors[0]
+    second_point = vectors[1]
+    sum_of_squared_delta = 0
 
-    for i in range(len(point1)):
-        delta = point1[i] - point2[i]
+    for i in range(len(first_point)):
+        delta = first_point[i] - second_point[i]
         squared = delta ** 2
-        sumOfSquaredDelta += squared
+        sum_of_squared_delta += squared
 
-    distance = math.sqrt(sumOfSquaredDelta)
+    distance = math.sqrt(sum_of_squared_delta)
 
     return distance
 
-def quickSort(arr, by):
+
+def quick_sort(arr, by):
+
     if len(arr) <= 1:
         return arr
 
-    pivot = arr[len(arr)//2][by]
+    pivot = arr[len(arr) // 2][by]
 
     # create array with all elements smaller than pivot
     left = [x for x in arr if x[by] < pivot]
@@ -91,7 +100,7 @@ def quickSort(arr, by):
     # create array with all elements greater than pivot
     right = [x for x in arr if x[by] > pivot]
 
-    sort_left = quickSort(left, by)
-    sort_right = quickSort(right, by)
+    sort_left = quick_sort(left, by)
+    sort_right = quick_sort(right, by)
 
     return sort_left + middle + sort_right
