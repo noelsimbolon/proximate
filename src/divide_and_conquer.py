9ff8460ed5e506e1fd32fa558ceab7e516df5fce
noelsimbolon <84700640+noelsimbolon@users.noreply.github.com>
2023-02-27 06:54:20 +0700

    refactor: finalize divide and conquer

--- src/divide_and_conquer.py
+++ src/divide_and_conquer.py
@@ -1,27 +1,41 @@
-import math
+from typing import Tuple
+
+import numpy as np
+
+import brute_force as bf
+import util
 
 
 # divide_by means divide by either x-axis or y-axis.
 # divide_by = 0 , represent divide by x
 # divide_by = 1, represent divide by y
-def find_closest_pair_dnc(vectors, dimension, divide_by=0):
-    if len(vectors) > 3:  # recurrence
+def find_closest_pair_dnc(points: np.ndarray[np.ndarray[float]], dimension: int, divide_by: int = 0) -> \
+        Tuple[Tuple[np.ndarray[float], np.ndarray[float]], float, int]:
+    """
+    Finds the closest pair of points using divide-and-conquer algorithm
+    :param points: a numpy array of points
+    :param dimension: the number of dimensions of the points
+    :param divide_by: the index of column to divide-and-conquer by
+    :return: a tuple: (the closest pair of points, its distance, number of Euclidean distance operations)
+    """
 
-        # sort vectors
-        sorted_vectors = quick_sort(vectors, divide_by)
+    if len(points) > 3:  # recurrence
+
+        # sort points
+        sorted_points = util.quick_sort(points, divide_by)
 
         # find index to divide
-        divide_at = len(sorted_vectors) // 2
+        divide_at = len(sorted_points) // 2
 
-        # split vectors
-        vectors_left = sorted_vectors[:divide_at]
-        vectors_right = sorted_vectors[divide_at:]
+        # split points
+        points_left = sorted_points[:divide_at]
+        points_right = sorted_points[divide_at:]
 
         # recurrence
-        closest_left, dist_left, count_left = find_closest_pair_dnc(vectors_left, dimension,
+        closest_left, dist_left, count_left = find_closest_pair_dnc(points_left, dimension,
                                                                     (divide_by + 1) %
                                                                     (dimension - 1 if dimension > 1 else dimension))
-        closest_right, dist_right, count_right = find_closest_pair_dnc(vectors_right, dimension,
+        closest_right, dist_right, count_right = find_closest_pair_dnc(points_right, dimension,
                                                                        (divide_by + 1) %
                                                                        (dimension - 1 if dimension > 1 else dimension))
 
@@ -30,77 +44,32 @@ def find_closest_pair_dnc(vectors, dimension, divide_by=0):
 
         # find if there were nearer points separated by the strip
         count_gray = 0
-        for i in range(len(sorted_vectors)):
-            for j in range(i + 1, len(sorted_vectors)):
+        for i in range(len(sorted_points)):
+            for j in range(i + 1, len(sorted_points)):
+
                 check_further = True
                 for k in range(dimension):
-                    if abs(sorted_vectors[i][k] - sorted_vectors[j][k]) > min_dist:
+
+                    if abs(sorted_points[i][k] - sorted_points[j][k]) > min_dist:
                         check_further = False
                         continue
+
                 if check_further:
-                    temp_dist = euclidean_distance((sorted_vectors[i], sorted_vectors[j]))
+                    temp_dist = util.euclidean_distance(sorted_points[i], sorted_points[j])
                     count_gray += 1
+
                     if temp_dist < min_dist:
-                        closest_vector, min_dist = (sorted_vectors[i], sorted_vectors[j]), temp_dist
+                        closest_vector, min_dist = (sorted_points[i], sorted_points[j]), temp_dist
 
         # return
         return closest_vector, min_dist, count_left + count_right + count_gray
 
-    elif len(vectors) == 3:  # basis 1
-        vect_temp1, dist1 = vectors[:2], euclidean_distance(vectors[:2])
-        vect_temp2, dist2 = vectors[1:3], euclidean_distance(vectors[1:3])
-        vect_temp3, dist3 = (vectors[0], vectors[2]), euclidean_distance((vectors[0], vectors[2]))
+    elif len(points) == 3:  # first basis
+        return bf.find_closest_pair_bf(points)
 
-        min_dist_temp = min(dist1, dist2, dist3)
+    else:  # second basis
+        # find distance between 2 points
+        dist = util.euclidean_distance(points[0], points[1])
 
-        if min_dist_temp == dist1:
-            return vect_temp1, dist1, 3
-        elif min_dist_temp == dist2:
-            return vect_temp2, dist2, 3
-        else:
-            return vect_temp3, dist3, 3
-
-    else:  # basis 2
-        # find distance between 2 vectors
-        dist = euclidean_distance(vectors)
-
-        # return the two vectors and distance between them
-        return vectors, dist, 1
-
-
-# distance of two vector
-def euclidean_distance(vectors):
-    first_point = vectors[0]
-    second_point = vectors[1]
-    sum_of_squared_delta = 0
-
-    for i in range(len(first_point)):
-        delta = first_point[i] - second_point[i]
-        squared = delta ** 2
-        sum_of_squared_delta += squared
-
-    distance = math.sqrt(sum_of_squared_delta)
-
-    return distance
-
-
-def quick_sort(arr, by):
-
-    if len(arr) <= 1:
-        return arr
-
-    pivot = arr[len(arr) // 2][by]
-
-    # create array with all elements smaller than pivot
-    left = [x for x in arr if x[by] < pivot]
-
-    # create array consists only pivot
-    middle = [x for x in arr if x[by] == pivot]
-
-    # create array with all elements greater than pivot
-    right = [x for x in arr if x[by] > pivot]
-
-    sort_left = quick_sort(left, by)
-    sort_right = quick_sort(right, by)
-
-    return sort_left + middle + sort_right
+        # return the two points and distance between them
+        return (points[0], points[1]), dist, 1
--- src/util.py
+++ src/util.py
@@ -1,14 +1,40 @@
 import math
+
 import numpy as np
 
 
 def euclidean_distance(first_point: np.ndarray[float], second_point: np.ndarray[float]) -> float:
     """
-    :param second_point:
-    :param first_point:
-    :return: the Euclidean distance between two vectors.
+    :param second_point: a numpy array of floats representing a single point
+    :param first_point: a numpy array of floats representing a single point
+    :return: the Euclidean distance between two points.
     """
     return math.sqrt(sum([(first_point[i] - second_point[i]) ** 2 for i in range(len(first_point))]))
+
+
+def quick_sort(points: np.ndarray[np.ndarray[float]], divide_by: int) -> np.ndarray[np.ndarray[float]]:
+    """
+    Performs quick sort to an array of points
+    :param points: a numpy array of points
+    :param divide_by: the index of column to sort by
+    :return: points sorted by a certain column (in this case, columns represent axes)
+    """
+
+    if len(points) <= 1:
+        return points
+
+    pivot = points[len(points) // 2][divide_by]
+
+    # Create array with all elements smaller than pivot
+    left = points[points[:, divide_by] < pivot]
+
+    # Create array consists only pivot
+    middle = points[points[:, divide_by] == pivot]
+
+    # Create array with all elements greater than pivot
+    right = points[points[:, divide_by] > pivot]
+
+    return np.concatenate((quick_sort(left, divide_by), middle, quick_sort(right, divide_by)))
 
 
 def validate_number_of_dimensions(d: str) -> bool:
